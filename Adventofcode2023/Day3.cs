461acb6e05929b07d3bd860ae8ffb6bf7f193404
Stefan <simanek.s@gmx.at>
2023-12-04 18:06:40 +0100

    day 3 + 4 done

---
+++ Adventofcode2023/Day3.cs
@@ -0,0 +1,118 @@
+﻿using Adventofcode2023.Util;
+
+namespace Adventofcode2023;
+
+public class Day3
+{
+    public static int SolvePart1()
+    {
+        var input = InputHandler.GetContent("day3.txt").ToArray();
+        var numbers = new List<Number>();
+        var symbols = new List<Symbol>();
+        for (var row = 0; row < input.Length; row++)
+        {
+            var currentNumber = new Number();
+            var digits = new List<int>();
+            for (var col = 0; col < input[row].Length; col++)
+            {
+                if (input[row][col] == '.')
+                {
+                    continue;
+                }
+                if (int.TryParse(input[row][col].ToString(), out var digit))
+                {
+                    digits.Add(digit);
+                    if (digits.Count == 1)
+                    {
+                        currentNumber.Start = (row, col);
+                    }
+                    while (col < input[row].Length - 1 && int.TryParse(input[row][col + 1].ToString(), out digit))
+                    {
+                        digits.Add(digit);
+                        col++;
+                    }
+                    currentNumber.End = (row, col);
+                    currentNumber.Value = int.Parse(string.Join("", digits));
+                    numbers.Add(currentNumber);
+                    currentNumber = new Number();
+                    digits.Clear();
+                }
+                else
+                {
+                    symbols.Add(new Symbol
+                    {
+                        Value = input[row][col],
+                        Position = (row, col)
+                    });
+                }
+            }
+        }
+        return numbers
+            .Where(number => symbols.Any(symbol => AreAdjacent(number, symbol)))
+            .Sum(number => number.Value);
+    }
+    public static int SolvePart2()
+    {
+        var input = InputHandler.GetContent("day3.txt").ToArray();
+        var numbers = new List<Number>();
+        var symbols = new List<Symbol>();
+        for (var row = 0; row < input.Length; row++)
+        {
+            var currentNumber = new Number();
+            var digits = new List<int>();
+            for (var col = 0; col < input[row].Length; col++)
+            {
+                if (input[row][col] == '.') { continue; }
+                if (int.TryParse(input[row][col].ToString(), out var digit))
+                {
+                    digits.Add(digit);
+                    if (digits.Count == 1)
+                    {
+                        currentNumber.Start = (row, col);
+                    }
+                    while (col < input[row].Length - 1 && int.TryParse(input[row][col + 1].ToString(), out digit))
+                    {
+                        digits.Add(digit);
+                        col++;
+                    }
+                    currentNumber.End = (row, col);
+                    currentNumber.Value = int.Parse(string.Join("", digits));
+                    numbers.Add(currentNumber);
+                    currentNumber = new Number();
+                    digits.Clear();
+                }
+                else
+                {
+                    symbols.Add(new Symbol
+                    {
+                        Value = input[row][col],
+                        Position = (row, col)
+                    });
+                }
+            }
+        }
+        return symbols
+            .Where(symbol => symbol.Value == '*')
+            .Select(symbol => numbers.Where(number => AreAdjacent(number, symbol)).ToArray())
+            .Where(gears => gears.Length == 2)
+            .Sum(gears => gears[0].Value * gears[1].Value);
+    }
+    private record Number
+    {
+        public int Value { get; set; }
+        public (int Row, int Column) Start { get; set; }
+        public (int Row, int Column) End { get; set; }
+    }
+
+    private record Symbol
+    {
+        public char Value { get; set; }
+        public (int Row, int Column) Position { get; set; }
+    }
+    private static bool AreAdjacent(Number number, Symbol symbol)
+    {
+        return Math.Abs(symbol.Position.Row - number.Start.Row) <= 1
+               && symbol.Position.Column >= number.Start.Column - 1
+               && symbol.Position.Column <= number.End.Column + 1;
+    }
+}
---
+++ Adventofcode2023/Day4.cs
@@ -0,0 +1,44 @@
+﻿using Adventofcode2023.Util;
+
+namespace Adventofcode2023;
+
+public static class Day4
+{
+    private static readonly char[] separator = [' '];
+    public static int SolvePart1()
+    {
+        int result = 0;
+        foreach (var item in InputHandler.GetContent("day4.txt"))
+        {
+            string[] parts = item[(item.IndexOf(':') + 1)..].Trim().Split('|');
+            result += (int)Math.Round(Math.Pow(2, parts[0].Trim().Split(separator, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()
+                .Count(parts[1].Trim().Split(separator, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList().Contains) - 1));
+        }
+        return result;
+    }
+    public static int SolvePart2()
+    {
+        int iter = 1;
+        Dictionary<int, int> gamesWithMatches = [];
+        Dictionary<int, int> finalCardCount = [];
+        foreach (var item in InputHandler.GetContent("day4.txt"))
+        {
+            string[] parts = item[(item.IndexOf(':') + 1)..].Trim().Split('|');
+            gamesWithMatches.Add(iter, parts[0].Trim().Split(separator, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()
+                .Count(parts[1].Trim().Split(separator, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList().Contains));
+            iter++;
+        }
+        foreach (var game in gamesWithMatches)
+        {
+            finalCardCount.Add(game.Key, 1);
+        }
+        foreach (var game in gamesWithMatches)
+        {
+            foreach (var nextGame in gamesWithMatches.Where(x => x.Key > game.Key && x.Key <= game.Key + game.Value))
+            {
+                finalCardCount[nextGame.Key] += finalCardCount[game.Key];
+            }
+        }
+        return finalCardCount.Sum(x => x.Value);
+    }
+}
