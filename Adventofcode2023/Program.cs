835628017897ae84d70242947d6ee2b66a0216bb
Stefan <simanek.s@gmx.at>
2023-12-09 17:54:22 +0100

    added interface(not used yet) started day6

--- Adventofcode2023/Program.cs
+++ Adventofcode2023/Program.cs
@@ -4,7 +4,7 @@ class AOC
 {
     static void Main()
     {
-        Console.WriteLine("Part 1: " + Day5.SolvePart1());
-        Console.WriteLine("Part 2: " + Day5.SolvePart2());
+        Console.WriteLine("Part 1: " + Day6.SolvePart1());
+        Console.WriteLine("Part 2: " + Day6.SolvePart2());
     }
 }
---
+++ Adventofcode2023/Util/ISolution.cs
@@ -0,0 +1,7 @@
+﻿namespace Adventofcode2023.Util;
+
+public interface ISolution
+{
+    public int SolvePart1();
+    public int SolvePart2();
+}
